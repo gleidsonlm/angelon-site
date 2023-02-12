3062f6f990632201b55a29545a1e54979975ec5b
GleidsonLM <gleidsonlm@gmail.com>
2023-02-11 22:45:24 -0300

    add: before tailwindcss

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -6,8 +6,8 @@ export default function Header() {
             <Image
                 src='logo.svg'
                 alt='Logo shows the styled name AngelOn where the l is I for input and O for output.'
-                width={100}
-                height={100}
+                width={200}
+                height={40}
             />
         </header>
     )
